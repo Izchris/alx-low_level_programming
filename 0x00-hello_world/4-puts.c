66fb50c014b960d14e86c6c7b47bd3150b15f3f9
Izchris <izabayochris@gmail.com>
2023-01-03 02:36:30 -0800

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * C program that prints and followed by a new line
+ * @src main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
